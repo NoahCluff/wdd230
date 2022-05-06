ed6ef63a3869ef8f0e1a28046f80b6a01aadb199
NoahCluff <clu16004@byui.edu>
2022-05-06 13:39:11 -0600

    a

--- js/sitescript.js
+++ js/sitescript.js
@@ -1,9 +1,9 @@
-let today = new Date();
+let today = new Date(document.lastModified);
 let dd = String(today.getDate()).padStart(2, '0');
 let mm = String(today.getMonth() + 1).padStart(2, '0');
 let yyyy = today.getFullYear();
 
 let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
-today = mm + "/" + dd + "/" + yyyy;
-document.getElementById("dateandtime").innerHTML = "Last Updated: " + today + " " + time;
+date = mm + "/" + dd + "/" + yyyy;
+document.getElementById("dateandtime").innerHTML = "Last Updated: " + date + " " + time;
